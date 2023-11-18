108765603def765f1ccfda712e3a7b6c2bc6e946
Felipeholguin <holguincalderon2006@gmail.com>
2023-11-18 16:48:56 -0500

    pagina-turismo

---
+++ src/Home/Home.jsx
@@ -0,0 +1,19 @@
+import './Home.css'
+import { Menu } from '../Menu/Menu'
+
+export function Home(){
+    return(
+        <>
+        <header>
+            <Menu>
+
+            </Menu>
+        </header>
+        <section className='banner'>
+            <p className='text-white'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa sapiente
+                 
+            </p>
+        </section>
+        </>
+    )
+}
---
+++ src/Usuario/Usuario.jsx
@@ -0,0 +1,11 @@
+export function Usuario(){
+    return(
+        <>
+       <div className="container">
+        <div className="row">
+            <div className="row-12"></div>
+        </div>
+       </div>
+        </>
+    )
+}
